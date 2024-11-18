c2dbe6d80c9476ff348e47ddae20e8824ab62997
Shwei <portal.gamy@gmail.com>
2024-11-19 00:40:32 +0200

    testing debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 //#include "PolynomialGenerator.hpp"
-//#include "PolynomialSolver.hpp"
+#include "PolynomialSolver.hpp"
 //#include "CirculantMatrix.hpp"
 //#include "GenerateCirculantMatrix.hpp"
 // C
@@ -19,33 +19,33 @@ int main(){
 //
 //cm.solve_and_output("../shweicomplex_matrix_solutions.txt");
 
-//double coefs[5] = {1.0, 0.0, -5.0, 0.0, 4.0};
-//C_PolynomialEquation* cpolynom = PolynomialEquation_create(coefs, 5);
-//
-//int degree = cpolynom->get_equation_type(cpolynom);
-//printf("Degree: %d\n", degree);
-//
-//std::cout << degree << std::endl;
-//
-//int root_count = 0;
-//c_double_complex* roots = cpolynom->solve(cpolynom, &root_count);
-//
-//if (roots) {
-//    std::cout << "Roots:" << std::endl;
-//    for (int i = 0; i < root_count; i++) {
-//        std::cout << "Root " << i + 1 << ": "
-//                  << roots[i].real() << " + " << roots[i].imag() << "i"
-//                  << std::endl;
-//    }
-//    free(roots);
-//} else {
-//    free(roots);
-//    std::cout << "No roots found." << std::endl;
-//}
-//
-//
-//PolynomialEquation_destroy(cpolynom);
+double coefs[5] = {0.0, -2.0, -40.0, -26.0, -41.0};
+C_PolynomialEquation* cpolynom = PolynomialEquation_create(coefs, 5);
 
-    C_PolynomialSolver("/Users/shwei/CLionProjects/ShweiCompleX/polynomials.txt", "../polynomials_complexh_out.txt");
+int degree = cpolynom->get_equation_type(cpolynom);
+printf("Degree: %d\n", degree);
+
+std::cout << degree << std::endl;
+
+int root_count = 0;
+c_double_complex* roots = cpolynom->solve(cpolynom, &root_count);
+
+if (roots) {
+    std::cout << "Roots:" << std::endl;
+    for (int i = 0; i < root_count; i++) {
+        std::cout << "Root " << i + 1 << ": "
+                  << roots[i].real() << " + " << roots[i].imag() << "i"
+                  << std::endl;
+    }
+    free(roots);
+} else {
+    free(roots);
+    std::cout << "No roots found." << std::endl;
+}
+
+
+PolynomialEquation_destroy(cpolynom);
+
+//    C_PolynomialSolver("/Users/shwei/CLionProjects/ShweiCompleX/polynomials.txt", "../polynomials_complexh_out.txt");
 
 }
